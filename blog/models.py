77b32dee471d1f5b0263c30744a7f5baea29ad4f
kelly <admin@admin.com>
2020-11-29 10:01:43 -0500

    Modified templates to display posts from database.

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+import datetime
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
